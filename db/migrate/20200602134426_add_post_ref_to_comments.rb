3308875143826eca6224f49dfc8c034bdf4316d6
hillarioh <okerioh@gmail.com>
2020-06-02 16:47:57 +0300

    add column and foreign constraint on comments table

---
+++ db/migrate/20200602134426_add_post_ref_to_comments.rb
@@ -0,0 +1,6 @@
+class AddPostRefToComments < ActiveRecord::Migration[6.0]
+  def change
+    add_column :comments,:post_id,:integer
+    add_foreign_key :comments,:posts
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_123035) do
+ActiveRecord::Schema.define(version: 2020_06_02_134426) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id"
-    t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
