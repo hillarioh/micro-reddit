d147ffe9a0ae9ff3a5b2eb514ae65f05b21a411f
hillarioh <okerioh@gmail.com>
2020-06-02 13:54:50 +0300

    add column and references

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 5 }
+    belongs_to :user
+    validates :title, presence: true, length: { minimum: 3 }
     validates :content, presence: true, length: { maximum: 500 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :posts
     validates :username, length: {minimum: 2}, presence: true, uniqueness: true
     validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
         message: "Please enter a valid email" }
