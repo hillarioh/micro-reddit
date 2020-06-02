f9df3af8dd7e0359f7ef3754b83638138406bb30
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-06-02 18:47:52 +0600

    Add comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_103408) do
+ActiveRecord::Schema.define(version: 2020_06_02_123035) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
